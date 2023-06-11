b7ae65c76d4a9be18ffa1df46b5b30cad725e85f
Aryan Verma <aryanverma@Aryans-MacBook-Air.local>
2023-06-11 22:24:14 +0530

    add Controller logic

--- src/main/java/com/blog_api/blog_api/BlogApiApplication.java
+++ src/main/java/com/blog_api/blog_api/BlogApiApplication.java
@@ -3,7 +3,7 @@ package com.blog_api.blog_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"services","controller","dao"})
 public class BlogApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/blog_api/blog_api/services/BlogPostService.java
+++ src/main/java/com/blog_api/blog_api/services/BlogPostService.java
@@ -2,6 +2,8 @@ package com.blog_api.blog_api.services;
 
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.blog_api.blog_api.dao.BlogPostDAO;
@@ -12,10 +14,12 @@ import com.blog_api.blog_api.entities.User;
 @Service
 public class BlogPostService {
 
+
     private final BlogPostDAO blogPostDAO;
     private final UserDAO userDAO;
 
     //setting an instance of the DAO interfaces to interact with JPA
+
     public BlogPostService(BlogPostDAO blogPostDAO, UserDAO userDAO) {
         this.blogPostDAO = blogPostDAO;
         this.userDAO = userDAO;
@@ -31,9 +35,35 @@ public class BlogPostService {
         return blogPostDAO.saveAndFlush(blogPost);
     }
 
-    public BlogPost editBlogPost(BlogPost updatedBlogPost) {
-        blogPostDAO.saveAndFlush(updatedBlogPost);
-        return updatedBlogPost;
+    public BlogPost editBlogPost(Long id, BlogPost updatedBlogPost) {
+        BlogPost blogPost = null;
+        for (BlogPost post : blogPostDAO.findAll()) {
+            if(post.getId() == id) {
+              blogPost = post;   
+            }
+        }
+        if(blogPost==null) {
+            return null;
+        }
+
+        return blogPostDAO.saveAndFlush(blogPost);
+    }
+
+    public boolean deleteBlogPost(Long id) {
+        if(blogPostDAO.existsById(id)) {
+            blogPostDAO.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+    public BlogPost getBlogPost(Long id) {
+        return blogPostDAO.findById(id).orElse(null);
+    }
+
+    public List<BlogPost> getAllBlogPosts(String username) {
+        User user = userDAO.findByUsername(username);
+        return blogPostDAO.findByUser(user);
+        
     }
     
 
