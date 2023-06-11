3d2b6a2487ed3101c2bc8767ae050d63844183b0
Aryan Verma <aryanverma@Aryans-MacBook-Air.local>
2023-06-11 23:03:14 +0530

    add user controller and service

--- src/main/java/com/blog_api/blog_api/BlogApiApplication.java
+++ src/main/java/com/blog_api/blog_api/BlogApiApplication.java
@@ -1,9 +1,16 @@
 package com.blog_api.blog_api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication(scanBasePackages = {"services","controller","dao"})
+
+
+
 public class BlogApiApplication {
 
 	public static void main(String[] args) {
